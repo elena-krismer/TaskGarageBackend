cb365c264551f2cf3043e9d6c75c6b2c58c341b7
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2026-02-04 07:36:42 +0100

    Add PATCH mapping to ignore nulls for DTOs

--- Mappings/MappingProfile.cs
+++ Mappings/MappingProfile.cs
@@ -9,15 +9,29 @@ public class MappingProfile : Profile
         // WorkOrder
         CreateMap<WorkOrder, WorkOrderReadDto>();
         CreateMap<WorkOrderCreateDto, WorkOrder>();
-        CreateMap<WorkOrderUpdateDto, WorkOrder>();
+
+        // PATCH mapping: ignore nulls 
+        CreateMap<WorkOrderUpdateDto, WorkOrder>()
+            .ForAllMembers(opt =>
+                opt.Condition((src, dest, srcMember) => srcMember != null));
+
+        CreateMap<WorkOrderPutDto, WorkOrder>();
 
         // Vehicle
         CreateMap<Vehicle, VehicleReadDto>();
         CreateMap<VehicleCreateDto, Vehicle>();
-        CreateMap<VehicleUpdateDto, Vehicle>();
+
+        // PATCH mapping: ignore nulls 
+        CreateMap<VehicleUpdateDto, Vehicle>()
+            .ForAllMembers(opt =>
+                opt.Condition((src, dest, srcMember) => srcMember != null));
 
         // Resource
         CreateMap<Resource, ResourceReadDto>();
         CreateMap<ResourceCreateDto, Resource>();
+
+        CreateMap<ResourceUpdateDto, Resource>()
+            .ForAllMembers(opt =>
+                opt.Condition((src, dest, srcMember) => srcMember != null));
     }
 }
